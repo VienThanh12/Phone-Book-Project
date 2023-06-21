740fd793df61a0307b524d0331018178fffacb7d
VienThanh <vienha000@gmail.com>
2023-06-21 15:53:47 +0700

    backend

--- index.js
+++ index.js
@@ -1,46 +1,59 @@
 const { request, response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+const cors = require('cors') // accest cors policy
 
 const app = express()
-app.use(cors())
+
+
+app.use(cors()) // cors policy
+app.use(express.static('build'))  // accest to front-end
 app.use(express.json())
 // https://github.com/expressjs/morgan#creating-new-tokens
 // Define a custom token for request body data
 morgan.token('body', (request) => JSON.stringify(request.body));
+
 app.use(morgan(':method :url :status :response-time ms - :body'));
 
-
-let notes = [
-  {
+let persons = [
+  { 
     "id": 1,
-    "content": "HTML is easy",
-    "important": true
+    "name": "Arto Hellas", 
+    "number": "040-123456"
   },
-  {
+  { 
     "id": 2,
-    "content": "Browser can execute only JavaScript",
-    "important": false
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
   },
-  {
+  { 
     "id": 3,
-    "content": "GET and POST are the most important methods of HTTP protocol",
-    "important": true
-  } 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,  
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
-app.get('/api/notes', (request, response) => {
-  response.send(notes)
+app.get('/api/persons', (request, response) => {
+  response.send(persons)
+})
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (request, response) => {
-  //response.send(notes)
+  //response.send(persons)
   const date = new Date()
   let day = date.toLocaleString('en-us', { weekday: 'long' });
   let month = date.toLocaleString('en-us', { month: 'long' });
   response.send(
-  `<p>Phonebook has information for ${notes.length} people</p> 
+  `<p>Phonebook has information for ${persons.length} people</p> 
   <p> ${date} </p>`
   )
   console.log(date)
@@ -50,9 +63,9 @@ const generatedID = () => {
   return Math.floor(Math.random() * 1000) + 1;
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body
-  const checkName = notes.find(person => person.name === body.name)
+  const checkName = persons.find(person => person.name === body.name)
   //console.log(checkName)
   if(!body.name || !body.number || checkName){
     return response.status(400).json({
@@ -64,13 +77,13 @@ app.post('/api/notes', (request, response) => {
     name: body.name,
     number: body.number
   }
-  notes = notes.concat(person)
+  persons = persons.concat(person)
   response.json(person)
 })
 
-app.get('/info/notes/:id', (request, response) => {
+app.get('/info/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const person = notes.find(note => note.id === id)
+  const person = persons.find(note => note.id === id)
   if(person) {
     response.json(person)
   } else {
@@ -78,14 +91,14 @@ app.get('/info/notes/:id', (request, response) => {
   }
 })
 
-app.delete('/info/notes/:id', (request, response) => {
+app.delete('/info/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(person => person.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
